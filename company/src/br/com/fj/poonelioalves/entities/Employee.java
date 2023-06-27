c293b3be95e96a067d6fa1fa80eec6f985845473
Flavio Milani <frmilanij@gmail.com>
2023-06-27 13:53:55 -0300

    Exercise added using polymorphis

---
+++ company/src/br/com/fj/poonelioalves/entities/Employee.java
@@ -0,0 +1,50 @@
+package br.com.fj.poonelioalves.entities;
+
+public class Employee {
+	private String name;
+	protected Integer hours;
+	protected double valuePerHour;
+	
+	public Employee() {
+	}
+
+	public Employee(String name, Integer hours, double valuePerHour) {
+		this.name = name;
+		this.hours = hours;
+		this.valuePerHour = valuePerHour;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getHours() {
+		return hours;
+	}
+
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+
+	public double getValuePerHour() {
+		return valuePerHour;
+	}
+
+	public void setValuePerHour(double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+	
+	public Double payment() {
+		return hours * valuePerHour;
+	}
+	
+	@Override
+	public String toString() {
+		return name + "- R$" + String.format("%.2f", payment());
+	}
+	
+}
